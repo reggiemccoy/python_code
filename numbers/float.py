58501b8dd27674b6b2cf20c08e6bfc3af5dbf513
reggiemccoy <45412294+reggiemccoy@users.noreply.github.com>
2019-01-21 21:40:25 -0600

    *number operations

--- numbers/float.py
+++ numbers/float.py
@@ -4,3 +4,5 @@ number1 = 5
 number2 = 2
 float_numbers = number1/number2
 print(float_numbers)
+
+
---
+++ numbers/precedence.py
@@ -0,0 +1,21 @@
+# Operator Precedence
+
+express = 5+2*3
+express2 = 10/2-3
+express3 = 8+12*2-4
+express4 = 6-3*2+7-1
+
+express5 = (5+2)*4
+express6 = 10/(5-3)
+express7 = 8+12*(6-2)
+express8 = (6-3)*(2+7)/3
+
+
+print(express)
+print(express2)
+print(express3)
+print(express4)
+print(express5)
+print(express6)
+print(express7)
+print(express8)
