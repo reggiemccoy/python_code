488607fb15dcda9d124ac448905d5950675dc6a1
reggiemccoy <45412294+reggiemccoy@users.noreply.github.com>
2019-01-21 21:51:53 -0600

    * average

---
+++ numbers/test_score_average.py
@@ -0,0 +1,17 @@
+# Get three test scores and assign them to the
+# test 1, test 2, and test3  variables
+
+
+test1 = float(input('Enter the first test score: '))
+test2 = float(input('Enter the second test score: '))
+test3 = float(input('Enter the third test score: '))
+
+
+# Calcualate the average of the three scores
+# and assign the result to the average variable.
+
+average =(test1 + test2 + test3) / 3.0
+
+# Display the average
+
+print(' The average score is ', average)
