d36a694d808ecd55a4ffac1f9e2fe2cb5fd03fd8
reggiemccoy <45412294+reggiemccoy@users.noreply.github.com>
2019-02-02 16:24:17 -0600

    * Sets

---
+++ List/sets.py
@@ -0,0 +1,10 @@
+# In mathematics, a set is a collection of elements where no element is repeated.
+# This becomes useful because if you know your data is stored in a set, you are g
+# guaranteed to have unique elements.
+
+
+names = set()
+names.add("Michele")
+names.add("Robin")
+names.add("Michele")
+print(names)
