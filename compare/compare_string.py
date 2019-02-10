ca6586916d06c77e8b9be4005c68b7cfc3f87bec
reggiemccoy <45412294+reggiemccoy@users.noreply.github.com>
2019-02-09 22:42:27 -0600

    * compare strings

---
+++ compare/compare_string.py
@@ -0,0 +1,10 @@
+# String compare  two strings
+
+str_x = 'Hello & Welcome'
+str_y = 'Hello & Welcome'
+
+# comparing by ==
+if str_x == str_y:
+    print("Same Strings")
+else:
+    print("Different Strings")
