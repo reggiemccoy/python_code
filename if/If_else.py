67172e007846b8b2bff00adf9e71c8197d2770b6
reggiemccoy <45412294+reggiemccoy@users.noreply.github.com>
2019-02-09 22:18:00 -0600

    * len function

--- if/If_else.py
+++ if/If_else.py
@@ -1,6 +1,6 @@
 #  In the code below I am coverting the input to a string
 
-age = int(input('enter your age'))
+age = int(input('enter your age  '))
 
 if age >= 18:
     print('You are an adult ')
@@ -8,3 +8,4 @@ if age >= 18:
 else:
 
     print('Your not an adult yet ')
+
---
+++ len/len.py
@@ -0,0 +1,6 @@
+
+# Below is an example I created of the len function
+ALI=("IamthegreatestAlisaid")
+x=len(ALI)
+
+print(x)
