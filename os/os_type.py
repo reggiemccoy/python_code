f0d3a8764b9878bfd7f6e13779d0747fd821364e
reggiemccoy <45412294+reggiemccoy@users.noreply.github.com>
2019-02-10 21:50:53 -0600

    * fixed OS bug and added more information about the OS  build

--- os/os_type.py
+++ os/os_type.py
@@ -1,2 +1,8 @@
 import os  # displays the OS information
-print(os.name)
+import platform
+
+
+print("OS type " + os.name)
+# print(platform.release())
+print(platform.system() +" " + platform.release())
+print( "Build number: " + platform.version())
