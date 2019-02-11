01aee16b20d9bf933946b2bc087e9b67c824e8d0
reggiemccoy <45412294+reggiemccoy@users.noreply.github.com>
2019-02-10 20:54:29 -0600

    * Added MAC address

--- network/computer_info.py
+++ network/computer_info.py
@@ -3,9 +3,18 @@
 # Python Program to Get IP Address
 import getpass
 import socket
+import uuid
+import pstats
+
 hostname = socket.gethostname()
 IPAddr = socket.gethostbyname(hostname)
+MacAddr = uuid.getnode()
 username = getpass.getuser()
-print("Your Computer Name is:" + hostname)
-print("Your Computer IP Address is:" + IPAddr)
-print(username)
+
+
+print("Your Computer Name is: " + hostname)
+print("Your Computer IP Address is: " + IPAddr)
+print("Your Computer MAC address is:  " + str(MacAddr))
+print("Your username is: " + username)
+
+
