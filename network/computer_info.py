eab06ff4c076793d67538d83d1b566915219a91c
reggiemccoy <45412294+reggiemccoy@users.noreply.github.com>
2019-02-10 21:04:03 -0600

    * Added Python version

--- network/computer_info.py
+++ network/computer_info.py
@@ -4,17 +4,21 @@
 import getpass
 import socket
 import uuid
+import sys
 
 
 hostname = socket.gethostname()
 IPAddr = socket.gethostbyname(hostname)
 MacAddr = uuid.getnode()
 username = getpass.getuser()
+PythonVer = sys.version
 
 
 print("Your Computer Name is: " + hostname)
+
 print("Your Computer IP Address is: " + IPAddr)
 print("Your Computer MAC address is:  " + str(MacAddr))
 print("Your username is: " + username)
+print("Python Version " + PythonVer)
 
 
