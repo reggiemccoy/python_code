d91382067a8e964d084e9ed8a1dd88c44a4bf197
reggiemccoy <45412294+reggiemccoy@users.noreply.github.com>
2019-02-14 16:40:32 -0600

    * Created a loop so that the computer information is updated every few seconds and added extra apace

--- network/computer_info.py
+++ network/computer_info.py
@@ -1,5 +1,4 @@
 
-
 # Python Program to Get IP Address
 import getpass
 import socket
@@ -9,9 +8,7 @@ import psutil # this had to be installed by pip
 import shutil
 import time
 import schedule # this had to be installed by pip
- 
-
-
+import os
 
 hostname = socket.gethostname()
 IPAddr = socket.gethostbyname(hostname)
@@ -22,22 +19,37 @@ CPUPercent = psutil.cpu_percent()
 MemVert = psutil.virtual_memory()
 NetworkPackets = psutil.net_io_counters()
 DiskUsage_info = shutil.disk_usage
+LaptopBattery = psutil.sensors_battery()
+
 
 
 def comp_info():  # Created a function for  all the computer information
 
     print("Your Computer Name is: " + hostname)
+
     print("Current CPU percentage: " + str(CPUPercent))
+
+    print("Current battery life if this system is a laptop: " + str(LaptopBattery))
+
+
+
     print("Your disk usage is: " + str(DiskUsage_info))
+
     print("Current Network Packets:" + str(NetworkPackets))
+
     print("Current Virtual memory :" + str(MemVert))
+
     print("Your Computer IP Address is: " + IPAddr)
+
     print("Your Computer MAC address is:  " + str(MacAddr))
+
     print("Your username is: " + username)
+
     print("Python Version " + PythonVer)
 
+    print("\n") # Added the following to add spaces between the  output refresh
 
-schedule.every(3).seconds.do(comp_info) # Created a schedule using a loop
+schedule.every(5).seconds.do(comp_info) # Created a schedule using a loop
 
 
 while 1:
@@ -45,6 +57,5 @@ while 1:
 
     schedule.run_pending()
     time.sleep(1)
-
 
 
