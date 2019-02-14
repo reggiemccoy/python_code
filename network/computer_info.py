713eb3bd1e67efb0dd3130eb1a4d1068481836e0
reggiemccoy <45412294+reggiemccoy@users.noreply.github.com>
2019-02-13 20:39:26 -0600

    * Created a loop so that the computer information is updated every few seconds

--- network/computer_info.py
+++ network/computer_info.py
@@ -6,6 +6,11 @@ import socket
 import uuid
 import sys
 import psutil # this had to be installed by pip
+import shutil
+import time
+import schedule # this had to be installed by pip
+ 
+
 
 
 hostname = socket.gethostname()
@@ -16,14 +21,30 @@ PythonVer = sys.version
 CPUPercent = psutil.cpu_percent()
 MemVert = psutil.virtual_memory()
 NetworkPackets = psutil.net_io_counters()
+DiskUsage_info = shutil.disk_usage
 
-print("Your Computer Name is: " + hostname)
-print("Current CPU percentage: " + str(CPUPercent))
-print("Current Network Packets:" + str(NetworkPackets))
-print("Current Virtual memory :" + str(MemVert))
-print("Your Computer IP Address is: " + IPAddr)
-print("Your Computer MAC address is:  " + str(MacAddr))
-print("Your username is: " + username)
-print("Python Version " + PythonVer)
+
+def comp_info():  # Created a function for  all the computer information
+
+    print("Your Computer Name is: " + hostname)
+    print("Current CPU percentage: " + str(CPUPercent))
+    print("Your disk usage is: " + str(DiskUsage_info))
+    print("Current Network Packets:" + str(NetworkPackets))
+    print("Current Virtual memory :" + str(MemVert))
+    print("Your Computer IP Address is: " + IPAddr)
+    print("Your Computer MAC address is:  " + str(MacAddr))
+    print("Your username is: " + username)
+    print("Python Version " + PythonVer)
+
+
+schedule.every(3).seconds.do(comp_info) # Created a schedule using a loop
+
+
+while 1:
+
+
+    schedule.run_pending()
+    time.sleep(1)
+
 
 
