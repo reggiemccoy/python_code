aed06745c37c0240175c676af1a31b26f40f467d
reggiemccoy <45412294+reggiemccoy@users.noreply.github.com>
2019-02-15 00:17:45 -0600

    * Combined pound and kilo program

---
+++ numbers/weightconvertor.py
@@ -0,0 +1,32 @@
+
+def metricTopound(kilograms):
+    pounds = kilograms * 2.2
+    ounces = pounds * 16
+
+    return int(pounds), ounces % 16
+
+
+def poundsToMetric(pounds):
+    kilograms = pounds / 2.2
+    grams = kilograms * 1000
+
+    return int(kilograms), grams % 1000
+
+
+weight = input('Enter K for kilos  or P for pounds : ')
+
+unit = weight
+
+if unit == "K":
+    kilograms = float(input("How many Kilos ? "))
+    lb, o = metricTopound(kilograms)
+    print("The amount of pounds you entered is {}. " \
+        "This is {} pounds and {}ounces.".format(kilograms, lb, o))
+
+elif unit == "P":
+    pounds = float(input("How many Pounds? "))
+    kg, g = poundsToMetric(pounds)
+    print("The amount of pounds you entered is {}. " \
+          "This is {} kilograms and {} grams.".format(pounds, kg, g))
+
+
