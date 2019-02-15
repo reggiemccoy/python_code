7619e6c56da33267a6e28487dc66a25a84fc2713
reggiemccoy <45412294+reggiemccoy@users.noreply.github.com>
2019-02-14 21:30:35 -0600

    * pounds to kilos

---
+++ numbers/weightconvert.py
@@ -0,0 +1,17 @@
+# The following program converts  pounds to kilos and kilos to pounds
+
+
+
+
+def poundsToMetric(pounds):
+    kilograms = pounds / 2.2
+    grams = kilograms * 1000
+    return int(kilograms), grams % 1000
+
+pounds = float(input("How many Pounds? "))
+kg, g = poundsToMetric(pounds)
+print('The amount of pounds you entered is {}. '\
+      'This is {} kilograms and {} grams.'.format(pounds, kg, g))
+
+
+
