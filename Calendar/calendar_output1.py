14de0ca5f36835fad6a2c979e7a840ecf4e59bf6
reggiemccoy <45412294+reggiemccoy@users.noreply.github.com>
2019-02-15 22:07:55 -0600

    * Input Calendar

---
+++ Calendar/calendar_output1.py
@@ -0,0 +1,9 @@
+import calendar
+
+# Note if the name of the file is  calendar then this script will fail
+
+y = int(input("Input the year : "))
+m = int(input("Input the month : "))
+
+print(calendar.month(y, m))
+
