6d7fd891601370bbd1adfc76d6f2aca1d4d51a03
reggiemccoy <45412294+reggiemccoy@users.noreply.github.com>
2019-08-03 00:59:52 -0500

    * Commented code

---
+++ numbers/percent_calc.py
@@ -0,0 +1,12 @@
+# Get the item's orginal price.
+
+orginal_price = float(input("Enter the items's original price !"))
+
+# Calculate the amount of the discount.
+discount = orginal_price * .02
+
+# Calculate the sale price
+sale_price = orginal_price - discount
+
+# Display the sale price
+print(' The sale price is', sale_price)
