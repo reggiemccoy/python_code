cfd0adc15f2b6480e81fb6b6f238083dfc5d7073
reggiemccoy <45412294+reggiemccoy@users.noreply.github.com>
2019-08-04 21:52:28 -0500

    * Commented code

---
+++ numbers/number_minimum.py
@@ -0,0 +1,4 @@
+# The following code minimizes the number to 12345.68
+# The 12 limits the field to a max of 12 characters
+
+print('The number is ', format(12345.6789, '12.2f'))
---
+++ string/multiple_lines.py
@@ -0,0 +1,1 @@
+print("Monday's sales are ")
