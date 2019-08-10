e393c1cf506df7be9fbe1dd8228297b8b13d6617
reggiemccoy <45412294+reggiemccoy@users.noreply.github.com>
2019-08-09 22:07:20 -0500

    * Commented code  added comments

--- numbers/money.py
+++ numbers/money.py
@@ -1,6 +1,10 @@
 c=int(input('Please enter an amount between 0-99:'))
 print(c//25, "quarters")
 c = c%25
+# //  Divides and returns the integer value of the quotient. It dumps the digits after the decimal.
+# %   Divides and returns the value of the remainder.
+
+
 print(c//10, "dimes")
 c = c%10
 print(c//5, "nickles")
