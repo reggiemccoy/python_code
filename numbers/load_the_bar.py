dfc8d5ee969ec017ed0d2de855cedc0bd1ed051f
reggiemccoy <45412294+reggiemccoy@users.noreply.github.com>
2019-08-10 16:51:33 -0500

    Currently working on a fix for the weight calculation  405 = eight 45, but 365 and 455 are wrong This is the second version.
    - Combined the two input statements into one line
    - Removed the variable subtraction by creating one input statement

--- numbers/load_the_bar.py
+++ numbers/load_the_bar.py
@@ -1,16 +1,10 @@
 
 
 b = int(input('Please enter weight to load on the bar: '))-int(input('Please enter bar weight : '))
-# bartype = int(input('Please enter bar weight: '))
 
-# b = (b - bartype)
-# x = bartype
-# b = (b-x)
-
-# print(b//45, "45's")
 print(int(b//45), "45's") if int(b//45)%2 == 0 else print(int(b//45)-1, "45's");
 
-#b = b%45
+b = b%45
 # //  Divides and returns the integer value of the quotient. It dumps the digits after the decimal.
 # %   Divides and returns the value of the remainder.
 
@@ -18,12 +12,12 @@ print(b//25, "25's")
 # print(int(b//25), "25's") if int(b//25)%2 == 0 else print(int(b//25)+1, "25's");
 b = b%25
 
-# print(b//10, "10's")
-print(int(b//10), "10's")
+print(b//10, "10's")
+# print(int(b//10), "10's")
 b = b%10
 
-# print(b//5, "5's")
-print(int(b//5), "5's")
+print(b//5, "5's")
+# print(int(b//5), "5's")
 b = b%5
 
 print(int(b/2.5), "2.5's")
