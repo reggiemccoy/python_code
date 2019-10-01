03638a91c39d7cea0d6cdf8eb1c6a168897ac13e
reggiemccoy <45412294+reggiemccoy@users.noreply.github.com>
2019-09-30 19:22:19 -0500

    Currently working on a fix for the weight calculation  405 = eight 45, but 365 and 455 are wrong This is the second version.
    - Combined the two input statements into one line
    - Removed the variable subtraction by creating one input statement

--- numbers/load_the_bar_V1.py
+++ numbers/load_the_bar_V1.py
@@ -3,15 +3,17 @@ b = int(input('Please enter weight to load on the bar: '))-int(input('Please ent
 
 print(int(b//45), "45's") if int(b//45)%2 == 0 else print(int(b//45)-1, "45's")
 
+a = b  # The following prints the total amount of weight without the bar
+print("Bar weight : " + str(a))
 b = b%45
+
 
 # //  Divides and returns the integer value of the quotient. It dumps the digits after the decimal.
 # %   Divides and returns the value of the remainder.
 
-x = b
+# x = b
 
 
-print(b//25, "25's")
-## print(int(b//25), "25's") if int(b//25)%2 == 0 else print(int(b//25)+1, "25's");
-x =x-b
+
+
 
